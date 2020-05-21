1fa46277b6739bcd3d1471972d4506daa9d7e521
Bruno Tavares <connect+github@bltavares.com>
2020-05-21 18:07:24 -0300

    Update certificates

--- kickstart/scripts/bootstrap.sh
+++ kickstart/scripts/bootstrap.sh
@@ -5,9 +5,9 @@ current_dir=$(dirname "${BASH_SOURCE[0]}")
 source "${current_dir}/../../secrets/env.sh"
 
 ## PVE
-#kickstart deploy root@"192.168.15.32" ssh-keys connection <<<"$NETWORK_ID"
-#kickstart deploy root@"192.168.15.3" bootstrap-debian ssh-keys
-#kickstart deploy --sudo bltavares@"192.168.15.3" docker-ce monitoring consul-server <../secrets/consul.key
+# kickstart deploy root@"192.168.15.32" ssh-keys connection <<<"$NETWORK_ID"
+# kickstart deploy root@"192.168.15.3" bootstrap-debian ssh-keys
+# kickstart deploy --sudo bltavares@"192.168.15.3" docker-ce monitoring consul-server <../secrets/consul.key
 # kickstart deploy --sudo bltavares@192.168.15.2 mediacenter
 # kickstart deploy --sudo bltavares@192.168.15.2 bouncer
 
@@ -22,34 +22,34 @@ source "${current_dir}/../../secrets/env.sh"
 # kickstart deploy --sudo bltavares@p4.zerotier.bltavares.com consul-client nomad-client <../secrets/consul.key
 
 # PVE: Debian vm
-#kickstart deploy root@192.168.15.193 bootstrap-debian ssh-keys
-#kickstart deploy --sudo bltavares@192.168.15.193 docker-ce connection <<<"$NETWORK_ID"
-#kickstart deploy --sudo bltavares@192.168.15.193 consul-client <../secrets/consul.key
+# kickstart deploy root@192.168.15.193 bootstrap-debian ssh-keys
+# kickstart deploy --sudo bltavares@192.168.15.193 docker-ce connection <<<"$NETWORK_ID"
+# kickstart deploy --sudo bltavares@192.168.15.193 consul-client <../secrets/consul.key
 # kickstart deploy --sudo bltavares@192.168.15.193 lab-web
 
 ## Controller
-#kickstart deploy --sudo bltavares@"192.168.15.245" consul-client <../secrets/consul.key
+# kickstart deploy --sudo bltavares@"192.168.15.245" consul-client <../secrets/consul.key
 
 ## Pi Zeros
 # P1
-#kickstart deploy --sudo bltavares@"192.168.15.159" consul-client <../secrets/consul.key
+# kickstart deploy --sudo bltavares@"192.168.15.159" consul-client <../secrets/consul.key
 
 # P2
-#kickstart deploy --sudo bltavares@"192.168.15.160" consul-client <../secrets/consul.key
+# kickstart deploy --sudo bltavares@"192.168.15.160" consul-client <../secrets/consul.key
 
 # P3
-#kickstart deploy --sudo bltavares@192.168.15.163 ssh-keys connection <<<"$NETWORK_ID"
-#kickstart deploy --sudo bltavares@"192.168.15.163" consul-client <../secrets/consul.key
+# kickstart deploy --sudo bltavares@192.168.15.163 ssh-keys connection <<<"$NETWORK_ID"
+# kickstart deploy --sudo bltavares@"192.168.15.163" consul-client <../secrets/consul.key
 
 # P4
-#kickstart deploy --sudo bltavares@192.168.15.162 ssh-keys connection <<<"$NETWORK_ID"
-#kickstart deploy --sudo bltavares@"192.168.15.162" consul-client <../secrets/consul.key
+# kickstart deploy --sudo bltavares@192.168.15.162 ssh-keys connection <<<"$NETWORK_ID"
+# kickstart deploy --sudo bltavares@"192.168.15.162" consul-client <../secrets/consul.key
 
 #Archiver
-#kickstart deploy root@"192.168.15.2" bootstrap-debian ssh-keys
-#kickstart deploy --sudo bltavares@"192.168.15.10" docker-ce connection <<<"$NETWORK_ID"
-#kickstart deploy --sudo bltavares@"192.168.15.10" fileserver mediacenter
-#kickstart deploy --sudo bltavares@"192.168.15.2" monitoring mediacenter
+# kickstart deploy root@"192.168.15.2" bootstrap-debian ssh-keys
+# kickstart deploy --sudo bltavares@"192.168.15.10" docker-ce connection <<<"$NETWORK_ID"
+# kickstart deploy --sudo bltavares@"192.168.15.10" fileserver mediacenter
+# kickstart deploy --sudo bltavares@"192.168.15.2" monitoring mediacenter
 
 # LibreElec
 #kickstart deploy --sudo bltavares@"192.168.15.2" consul-server <../secrets/consul.key
@@ -61,4 +61,8 @@ source "${current_dir}/../../secrets/env.sh"
 # Open Wisp
 # kickstart deploy root@"192.168.15.111" bootstrap-debian ssh-keys
 # kickstart deploy --sudo bltavares@"192.168.15.111" docker-ce connection <<<"$NETWORK_ID"
-kickstart deploy --sudo bltavares@"192.168.15.111" openwisp
+# kickstart deploy --sudo bltavares@"192.168.15.111" openwisp
+
+# With certificates
+# kickstart deploy --sudo bltavares@192.168.15.2 bouncer
+# kickstart deploy --sudo bltavares@192.168.15.193 lab-web
