5f32738cf755cf388f962584bb044c68b46efe0f
Bruno Tavares <connect+github@bltavares.com>
2023-08-23 22:14:47 -0300

    Adjust docker installation script to work on opensuse

--- kickstart/roles/docker-ce.sh
+++ kickstart/roles/docker-ce.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
-packages=(
-    apt-transport-https
-    ca-certificates
-    curl
-    gnupg2
-    software-properties-common
-)
+kickstart.context "Docker CE"
 
-for package in ${packages[*]}; do
-    kickstart.package.install "$package"
-done
+if [[ $(kickstart.package.manager) == "apt" ]]; then
+    packages=(
+        apt-transport-https
+        ca-certificates
+        curl
+        gnupg2
+        software-properties-common
+    )
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+    for package in ${packages[*]}; do
+        kickstart.package.install "$package"
+    done
 
-if ! [[ -f /etc/apt/sources.list.d/docker-ce.list ]]; then
-    cat >/etc/apt/sources.list.d/docker-ce.list <<<"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-    kickstart.package.update
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+
+    if ! [[ -f /etc/apt/sources.list.d/docker-ce.list ]]; then
+        cat >/etc/apt/sources.list.d/docker-ce.list <<<"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+        kickstart.package.update
+    fi
+
+    packages=(docker-ce docker-ce-cli containerd.io)
+    for package in ${packages[*]}; do
+        kickstart.package.install "$package"
+    done
+elif [[ $(kickstart.package.manager) == "zypper" ]]; then
+    packages=(
+        docker containerd docker-compose
+    )
+    for package in ${packages[*]}; do
+        kickstart.package.install "$package"
+    done
 fi
-
-packages=(docker-ce docker-ce-cli containerd.io)
-for package in ${packages[*]}; do
-    kickstart.package.install "$package"
-done
 
 if kickstart.user.exists bltavares; then
     kickstart.user.add_group bltavares docker
 fi
+
+kickstart.service.enable docker
+kickstart.service.start docker
 
 if ! [[ -f /etc/docker/daemon.json ]]; then
     cp -f files/docker/daemon.json /etc/docker/daemon.json
