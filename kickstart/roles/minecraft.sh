352d15d7521e979241a87449e4a705bdea386c94
Bruno Tavares <connect+github@bltavares.com>
2025-01-05 14:19:25 -0300

    minecraft: Update recipe to latest and include multi-server setup

--- kickstart/roles/minecraft.sh
+++ kickstart/roles/minecraft.sh
@@ -3,37 +3,75 @@
 set -e
 kickstart.context "Minecraft Java/Bedrock Server"
 
-if ! kickstart.command_exists godns; then
-  (
-    cd /tmp
-    wget -O godns.tar.gz https://github.com/TimothyYe/godns/releases/download/V2.3/godns-linux64-2.3.tar.gz
-    tar xvf godns.tar.gz
-    mv godns /usr/local/bin/godns
-  )
-fi
+# Tunnel for gaming as Cloudflare only supports https
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+sudo apt update
+sudo apt install playit
 
-# cp files/godns.service /lib/systemd/system/godns.service
-# cp files/secrets/minecraft/godns.json /etc/godns.json
-# kickstart.service.enable godns
-# kickstart.service.start godns
+## TODO add [besta-fera](https://github.com/bltavares/besta-fera) setup
 
-# cp files/godns6.service /lib/systemd/system/godns6.service
-# cp files/secrets/minecraft/godns6.json /etc/godns6.json
-# kickstart.service.enable godns6
-# kickstart.service.start godns6
-
-docker pull bltavares/minecraft:1.19.2
-docker rm -f minecraft || true
-docker run -d -ti --name minecraft \
-  -e EULA=TRUE \
-  -e SERVER_NAME="Familia Corrosiva" \
-  -e MOTD="Venha se divertir" \
-  -e DIFFICULTY=peaceful \
-  -e MODE=creative \
-  -e WHITELIST="bltavares,*bltavares" \
-  -e OPS="bltavares,*bltavares" \
-  -v /opt/minecraft:/data \
+docker pull itzg/mc-proxy:java21
+docker rm -f velocity || true
+docker run -d -ti --name velocity \
+  -e TYPE=VELOCITY \
+  -e ONLINE_MODE=TRUE \
+  -e SERVER_PORT="25565" \
+  -v /opt/velocity:/server \
   --restart=unless-stopped \
   --network host \
-  bltavares/minecraft:1.19.2
+  itzg/mc-proxy:java21
+
+docker pull itzg/minecraft-server:java21
+
+docker rm -f creative || true
+docker run -d -ti --name creative \
+  -e TYPE=PAPER \
+  -e PAPER="1.21.3" \
+  -e ONLINE_MODE=FALSE \
+  -e EULA=TRUE \
+  -e ENABLE_AUTOPAUSE=TRUE \
+  -e JVM_DD_OPTS="disable.watchdog=true" \
+  -e MAX_TICK_TIME="-1" \
+  -v /opt/creative:/data \
+  --restart=unless-stopped \
+  -p "25511:25565" \
+  itzg/minecraft-server:java21
+
+docker rm -f oneblock || true
+docker run -d -ti --name oneblock \
+  -e TYPE=PAPER \
+  -e PAPER="1.21.3" \
+  -e ONLINE_MODE=FALSE \
+  -e EULA=TRUE \
+  -e ENABLE_AUTOSTOP=TRUE \
+  --restart=on-failure \
+  -v /opt/oneblock:/data \
+  -p "25512:25565" \
+  itzg/minecraft-server:java21
+
+docker rm -f skyblock || true
+docker run -d -ti --name skyblock \
+  -e TYPE=PAPER \
+  -e PAPER="1.21.3" \
+  -e ONLINE_MODE=FALSE \
+  -e EULA=TRUE \
+  -e ENABLE_AUTOSTOP=TRUE \
+  --restart=on-failure \
+  -v /opt/skyblock:/data \
+  -p "25513:25565" \
+  itzg/minecraft-server:java21
+
+docker rm -f survival || true
+docker run -d -ti --name survival \
+  -e TYPE=PAPER \
+  -e PAPER="1.21.3" \
+  -e ONLINE_MODE=FALSE \
+  -e EULA=TRUE \
+  -e ENABLE_AUTOSTOP=TRUE \
+  --restart=on-failure \
+  -v /opt/survival:/data \
+  -p "25514:25565" \
+  itzg/minecraft-server:java21
+
 docker system prune -f
