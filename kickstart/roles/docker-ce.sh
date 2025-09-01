fec6ce3fa8a009daadd3e097dad1d53782af2843
Bruno Tavares <connect+github@bltavares.com>
2025-09-01 16:48:26 -0300

    Update docker installation script to support new APT list format on Debian 13 Trixie

--- kickstart/roles/docker-ce.sh
+++ kickstart/roles/docker-ce.sh
@@ -3,48 +3,53 @@
 kickstart.context "Docker CE"
 
 if [[ $(kickstart.package.manager) == "apt-get" ]]; then
-    packages=(
-        apt-transport-https
-        ca-certificates
-        curl
-        gnupg2
-        software-properties-common
-    )
+	packages=(
+		ca-certificates
+		curl
+		gnupg2
+	)
 
-    for package in ${packages[*]}; do
-        kickstart.package.install "$package"
-    done
+	for package in ${packages[*]}; do
+		kickstart.package.install "$package"
+	done
 
-    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+	if ! [[ -f /etc/apt/sources.list.d/docker-ce.sources ]]; then
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor --yes -o /etc/apt/keyrings/docker.asc
 
-    if ! [[ -f /etc/apt/sources.list.d/docker-ce.list ]]; then
-        cat >/etc/apt/sources.list.d/docker-ce.list <<<"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-        kickstart.package.update
-    fi
+		cat >/etc/apt/sources.list.d/docker-ce.sources <<EOD
+Types: deb
+URIs: https://download.docker.com/linux/debian/
+Suites: trixie
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOD
 
-    packages=(docker-ce docker-ce-cli containerd.io)
-    for package in ${packages[*]}; do
-        kickstart.package.install "$package"
-    done
+		kickstart.package.update
+	fi
+
+	packages=(docker-ce docker-ce-cli containerd.io)
+	for package in ${packages[*]}; do
+		kickstart.package.install "$package"
+	done
 elif [[ $(kickstart.package.manager) == "zypper" ]]; then
-    packages=(
-        docker containerd docker-compose
-    )
-    for package in ${packages[*]}; do
-        kickstart.package.install "$package"
-    done
+	packages=(
+		docker containerd docker-compose
+	)
+	for package in ${packages[*]}; do
+		kickstart.package.install "$package"
+	done
 fi
 
 if kickstart.user.exists bltavares; then
-    kickstart.user.add_group bltavares docker
+	kickstart.user.add_group bltavares docker
 fi
 
 kickstart.service.enable docker
 kickstart.service.start docker
 
 if ! [[ -f /etc/docker/daemon.json ]]; then
-    cp -f files/docker/daemon.json /etc/docker/daemon.json
-    kickstart.service.restart docker
+	cp -f files/docker/daemon.json /etc/docker/daemon.json
+	kickstart.service.restart docker
 fi
 
 docker ps
